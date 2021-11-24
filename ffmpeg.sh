9b118d4ac7e7276c95b6c86101fe940f8b90ec2f
Mr. Kin <im.misterkin@gmail.com>
2021-11-24 12:28:52 +0800

    feat: support image2webp

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -7,7 +7,7 @@ ffmpeg_no_banner() {
 preparational_work() {
     clear
     echo "功能：$1"
-    echo "输入 0 则返回主菜单"
+    echo "提示：输入0回车，则返回主菜单"
     local path
     read -r -p "请输入路径并回车：" path
     if [ "$path" = "0" ]; then
@@ -35,6 +35,12 @@ preparational_work() {
 
 finished_work() {
     echo "已结束操作"
+    if [ $# -eq 1 ]; then
+        echo "请查看$1文件夹"
+    fi
+    if [ $# -eq 2 ]; then
+        echo "请查看$1文件夹和$2文件夹"
+    fi
     cd ~- || return
     echo "已切换回上一次的工作路径"
 }
@@ -63,7 +69,7 @@ image_add_watermark() {
     done
     shopt -u nullglob
 
-    finished_work
+    finished_work "$output_path1" "$output_path2"
 }
 
 image_sequence_to_video_with_gpu() {
@@ -75,18 +81,61 @@ image_sequence_to_video_with_gpu() {
     fi
 
     local name_length
-    read -r -p "请输入图片序列名的长度（不输入直接回车则默认长度为4）：" name_length
-    if [ "$name_length" == "" ]; then
+    echo "提示：不输入（等待10s）或直接回车，则默认长度为4，如0001.png"
+    if read -t 10 -r -p "请输入图片序列名的长度：" name_length; then
+        if [ "$name_length" == "" ]; then
+            name_length=4
+        fi
+    else
+        echo
         name_length=4
     fi
-    ffmpeg_no_banner -hwaccel cuda -hwaccel_output_format cuda -r 24 -f image2 -i %0"$name_length"d.png -r 24 -c:v h264_nvenc -profile:v high -preset slow -rc:v vbr -cq:v 19 -b:v 2500k -maxrate:v 5000k "output/output.mp4"
+    local video_bitrate video_maxrate
+    echo "提示：不输入（等待10s）或直接回车，则默认为8M 10M，用空格分割两个参数，如2.5M 5M"
+    if read -t 10 -r -p "请输入压制视频的码率和最大码率：" video_bitrate video_maxrate; then
+        if [ "$video_bitrate" = "" ]; then
+            video_bitrate="8M"
+        fi
+        if [ "$video_maxrate" = "" ]; then
+            video_maxrate="10M"
+        fi
+    else
+        echo
+        video_bitrate="8M"
+        video_maxrate="10M"
 
-    finished_work
+    fi
+    png_count=$(ls -1 *.png 2>/dev/null | wc -l)
+    if [ $png_count -gt 0 ]; then
+        ffmpeg_no_banner -hwaccel cuda -hwaccel_output_format cuda -r 24 -f image2 -i %0"$name_length"d.png -r 24 -c:v h264_nvenc -profile:v high -preset:v slow -rc:v vbr -cq:v 19 -b:v "$video_bitrate" -maxrate:v "$video_maxrate" "output/output.mp4"
+    else
+        ffmpeg_no_banner -hwaccel cuda -hwaccel_output_format cuda -c:v mjpeg_cuvid -r 24 -f image2 -i %0"$name_length"d.jpg -r 24 -c:v h264_nvenc -profile:v high -preset:v slow -rc:v vbr -cq:v 19 -b:v "$video_bitrate" -maxrate:v "$video_maxrate" "output/output.mp4"
+    fi
+
+    finished_work "$output_path1"
+}
+
+image_to_webp() {
+    local description="压缩图片，全部转为webp格式"
+    local output_path1="output"
+    preparational_work "$description" "$output_path1"
+    if [ $? -eq 10 ]; then
+        return 0
+    fi
+
+    for file in *.png *.jpg; do
+        ffmpeg_no_banner -i "$file" "$output_path1/${file%.*}.webp"
+    done
+    for file in *.gif; do
+        ffmpeg_no_banner -i "$file" "$output_path1/${file%.*}.webp"
+    done
+
+    finished_work "$output_path1"
 }
 
 test() {
     local description="测试"
-    local output_path1="for_web"
+    local output_path1="output"
     preparational_work "$description" "$output_path1"
     if [ $? -eq 10 ]; then
         return 0
@@ -97,12 +146,16 @@ test() {
 
 while true; do
     echo "========================================"
-    options=("给图片添加版权水印并压缩" "显卡加速将图片序列合成为视频" "退出程序" "测试")
+    options=("给图片添加版权水印并压缩" "显卡加速将图片序列合成为视频" "压缩图片，全部转为webp格式" "退出程序" "测试")
     PS3="请选择菜单："
     select option in "${options[@]}"; do
         case $option in
         "测试")
             test
+            break
+            ;;
+        "压缩图片，全部转为webp格式")
+            image_to_webp
             break
             ;;
         "显卡加速将图片序列合成为视频")
