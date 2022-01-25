d00608444f147a630288b94c9af9d1cba69a62dd
Mr. Kin <im.misterkin@gmail.com>
2022-01-25 14:24:41 +0800

    style: improve hint message

--- common.sh
+++ common.sh
@@ -39,12 +39,12 @@ change_directory() {
     draw_line "-"
     echo "提示：输入0并回车，则返回主菜单"
     local path
-    read -r -p "请输入路径并回车：" path
+    read -r -p "请输入文件夹路径并回车：" path
     if [ "$path" = "0" ]; then
         return 10
     fi
     while ! cd "$path" 2>/dev/null || [ "$path" = "" ]; do
-        read -r -p "当前输入路径不存在，请重新输入路径并回车：" path
+        read -r -p "当前输入路径不存在，请重新输入文件夹路径并回车：" path
         if [ "$path" = "0" ]; then
             return 10
         fi
