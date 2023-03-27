ebf36cc1b56633310aa626bb9462605fb0fe485f
Mr. Kin <im.misterkin@gmail.com>
2023-03-28 00:40:52 +0800

    feat: support for deleting audio album

--- ffmpeg_modules/audio_album.sh
+++ ffmpeg_modules/audio_album.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# to-do：删除音频封面
+
 audio_album() {
     while true; do
         clear
         draw_line_echo "="
         echo -e "FFmpeg批处理工具子菜单：\n\n处理音频封面图：获取、添加、删除\n"
-        options=("获取音频封面图" "添加音频封面图" "退出子菜单")
+        options=("获取音频封面图" "添加音频封面图" "删除音频封面图" "退出子菜单")
         PS3=$'\n请选择子菜单：'
         select option in "${options[@]}"; do
             case $option in
@@ -18,6 +18,12 @@ audio_album() {
             "添加音频封面图")
                 while [ $? -ne 20 ]; do
                     audio_album_attach
+                done
+                break
+                ;;
+            "删除音频封面图")
+                while [ $? -ne 20 ]; do
+                    audio_album_delete
                 done
                 break
                 ;;
---
+++ ffmpeg_modules/audio_album_delete.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+audio_album_delete() {
+    local output_path="audio_album_delete"
+    local feature_name feature_intro feature_note
+    feature_name="删除音频封面图"
+    feature_intro="删除所有mp3文件或者m4a文件或者flac文件的音频封面图"
+    feature_note="$(description_append_note "option_false" "directory" "$output_path")"
+    description "$feature_name" "$feature_intro" "$feature_note"
+    change_directory
+    if [ $? -eq 10 ]; then
+        return 20
+    fi
+
+    local all_count m4a_count mp3_count flac_count
+    m4a_count=$(file_count "m4a")
+    mp3_count=$(file_count "mp3")
+    flac_count=$(file_count "flac")
+    all_count=$(("$m4a_count" + "$mp3_count" + "$flac_count"))
+    if [ "$all_count" -eq 0 ]; then
+        log_file_not_detected "m4a" "mp3" "flac"
+        return 0
+    fi
+
+    log_start
+    make_directory "$output_path"
+    local operation_count=0
+    shopt -s nullglob
+    local media_stream_number
+    for file in $(file_extension_for_loop "mp3" "m4a" "flac"); do
+        media_stream_number=$(get_media_info "$file" "format=nb_streams")
+        draw_line_echo "~"
+        if [ "$media_stream_number" -eq 2 ]; then
+            # -map 0 选择所有流；-map -0:v 选择音频（-0:v加负号是取消选择视频流，即反选）
+            ffmpeg_no_banner -i "$file" -map 0 -map -0:v -c copy "$output_path/$file"
+            ((operation_count++))
+            echo
+        else
+            text_echo "「$file」文件中没有图像，无需进行删除音频封面图操作"
+        fi
+    done
+    log_end "$operation_count" "$all_count"
+    log_result "option_false" "directory" "$output_path"
+    shopt -u nullglob
+}
