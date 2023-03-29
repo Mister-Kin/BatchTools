7ddedd895a1fd09daf365c02c75572ac68ae6e36
Mr. Kin <im.misterkin@gmail.com>
2023-03-30 04:29:24 +0800

    feat: set title of metadata

--- ffmpeg_modules/audio_metadata.sh
+++ ffmpeg_modules/audio_metadata.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
 audio_metadata() {
+    local -a menu_options_array
+    menu_options_array+=("根据元数据标签重命名音频文件")
+    menu_options_array+=("根据音频文件名修改元数据标签")
+    menu_options_array+=("设置音频文件的专辑名")
+    menu_options_array+=("设置音频文件的歌曲名")
+    menu_options_array+=("退出子菜单")
     while true; do
         clear
         draw_line_echo "="
-        echo -e "FFmpeg批处理工具子菜单：\n\n音频元数据标签工具\n"
-        options=("根据元数据标签重命名音频文件" "根据音频文件名修改元数据标签" "设置音频文件的专辑名" "退出子菜单")
-        PS3=$'\n请选择子菜单：'
-        select option in "${options[@]}"; do
-            case $option in
-            "设置音频文件的专辑名")
-                while [ $? -ne 20 ]; do
-                    audio_metadata_set_album
-                done
-                break
-                ;;
-            "根据元数据标签重命名音频文件")
-                while [ $? -ne 20 ]; do
-                    audio_metadata_rename_file
-                done
-                break
-                ;;
-            "根据音频文件名修改元数据标签")
-                while [ $? -ne 20 ]; do
-                    audio_metadata_by_file_name
-                done
-                break
-                ;;
-            "退出子菜单")
-                return 20
-                ;;
-            *)
-                echo -e "\n错误的选项，请重新选择子菜单" >&2
-                ;;
-            esac
-        done
+        echo -e "FFmpeg批处理工具子菜单 —— 音频元数据标签工具\n"
+        echo "提示：使用上下方向键↑↓选择，回车键Enter确认选项："
+        echo -e "\n请选择子菜单：\n"
+        arrow_select_option "${menu_options_array[@]}"
+        local choice=$?
+        case ${menu_options_array[$choice]} in
+        "设置音频文件的歌曲名")
+            while [ $? -ne 20 ]; do
+                audio_metadata_set_title
+            done
+            ;;
+        "设置音频文件的专辑名")
+            while [ $? -ne 20 ]; do
+                audio_metadata_set_album
+            done
+            ;;
+        "根据元数据标签重命名音频文件")
+            while [ $? -ne 20 ]; do
+                audio_metadata_rename_file
+            done
+            ;;
+        "根据音频文件名修改元数据标签")
+            while [ $? -ne 20 ]; do
+                audio_metadata_by_file_name
+            done
+            ;;
+        "退出子菜单")
+            return 20
+            ;;
+        esac
     done
 }
---
+++ ffmpeg_modules/audio_metadata_set_title.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+audio_metadata_set_title() {
+    local output_path="audio_metadata_set_title"
+    local feature_name feature_intro feature_note
+    feature_name="设置音频文件的歌曲名"
+    feature_intro="设置路径下的mp3文件或者m4a文件或者flac文件元数据标签的歌曲名$(description_append_intro "设置元数据标签-歌曲名")"
+    feature_note="$(description_append_note "option_false" "directory" "$output_path")"
+    description "$feature_name" "$feature_intro" "$feature_note"
+    change_directory
+    if [ $? -eq 10 ]; then
+        return 20
+    fi
+
+    local all_count m4a_count mp3_count flac_count
+    m4a_count=$(file_count "m4a")
+    mp3_count=$(file_count "mp3")
+    flac_count=$(file_count "flac")
+    all_count=$(("$m4a_count" + "$mp3_count" + "$flac_count"))
+    if [ "$all_count" -eq 0 ]; then
+        log_file_not_detected "m4a" "mp3" "flac"
+        return 0
+    fi
+
+    log_start
+    make_directory "$output_path"
+    local operation_count=0
+    shopt -s nullglob
+    local file
+    if [ "$all_count" -eq 1 ]; then
+        for audio_file in $(file_extension_for_loop "mp3" "m4a" "flac"); do
+            file="$audio_file"
+        done
+    else
+        draw_line_echo "~"
+        text_echo "提示：使用上下方向键↑↓选择文件，回车键Enter确认选项"
+        text_echo "当前路径下检测到多个音频文件"
+        text_echo "现在进入手动选择音频文件模式，请选择需要修改元数据标签-歌曲名的音频："
+        local -a audio_file_array=()
+        if [ "$m4a_count" -ne 0 ]; then
+            audio_file_array+=(*.m4a)
+            audio_file_array+=(*.M4A)
+        fi
+        if [ "$mp3_count" -ne 0 ]; then
+            audio_file_array+=(*.mp3)
+            audio_file_array+=(*.MP3)
+        fi
+        if [ "$flac_count" -ne 0 ]; then
+            audio_file_array+=(*.flac)
+            audio_file_array+=(*.FLAC)
+        fi
+        audio_file_array+=("取消功能操作，返回菜单")
+        arrow_select_option "${audio_file_array[@]}"
+        local choice=$?
+        local choice_exit=$((${#audio_file_array[@]} - 1))
+        if [ $choice -eq $choice_exit ]; then
+            return 20
+        fi
+        file="${audio_file_array[$choice]}"
+    fi
+
+    local audio_title
+    audio_title=$(input_anything "请输入元数据标签-歌曲名")
+    if [ $? -eq 10 ]; then
+        return 20
+    fi
+    draw_line_echo "~"
+    text_echo "当前已设置元数据标签-歌曲名为「$audio_title」"
+
+    ffmpeg_no_banner -i "$file" -c copy -map_chapters -1 -metadata title="$audio_title" "$output_path/$file"
+    ((operation_count++))
+    draw_line_echo "~"
+    show_progress_bar "1" "$operation_count"
+    log_end "$operation_count" "$all_count"
+    log_result "$feature_note"
+    shopt -u nullglob
+}
