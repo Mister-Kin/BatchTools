2062b5aeda9c7d6ef2d236c8eb85dab3494fdb7c
Mr. Kin <im.misterkin@gmail.com>
2023-03-29 19:13:06 +0800

    feat: set album of metadata

--- ffmpeg_modules/audio_metadata.sh
+++ ffmpeg_modules/audio_metadata.sh
@@ -5,10 +5,22 @@ audio_metadata() {
         clear
         draw_line_echo "="
         echo -e "FFmpeg批处理工具子菜单：\n\n音频元数据标签工具\n"
-        options=("根据音频文件名修改元数据标签" "退出子菜单")
+        options=("根据元数据标签重命名音频文件" "根据音频文件名修改元数据标签" "设置音频文件的专辑名" "退出子菜单")
         PS3=$'\n请选择子菜单：'
         select option in "${options[@]}"; do
             case $option in
+            "设置音频文件的专辑名")
+                while [ $? -ne 20 ]; do
+                    audio_metadata_set_album
+                done
+                break
+                ;;
+            "根据元数据标签重命名音频文件")
+                while [ $? -ne 20 ]; do
+                    audio_metadata_rename_file
+                done
+                break
+                ;;
             "根据音频文件名修改元数据标签")
                 while [ $? -ne 20 ]; do
                     audio_metadata_by_file_name
---
+++ ffmpeg_modules/audio_metadata_set_album.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+audio_metadata_set_album() {
+    local output_path="audio_metadata_set_album"
+    local feature_name feature_intro feature_note
+    feature_name="设置音频文件的专辑名"
+    feature_intro="设置路径下的mp3文件或者m4a文件或者flac文件元数据标签的专辑名$(description_append_intro "设置元数据标签-专辑名")"
+    feature_note="$(description_append_note "option_false" "directory" "$output_path")"
+    description "$feature_name" "$feature_intro" "$feature_note"
+    change_directory
+    if [ $? -eq 10 ]; then
+        return 20
+    fi
+
+    local all_count m4a_count mp3_count flac_count
+    m4a_count=$(file_count "m4a")
+    mp3_count=$(file_count "mp3")
+    flac_count=$(file_count "flac")
+    all_count=$(("$m4a_count" + "$mp3_count" + "$flac_count"))
+    if [ "$all_count" -eq 0 ]; then
+        log_file_not_detected "m4a" "mp3" "flac"
+        return 0
+    fi
+
+    local audio_ablum
+    audio_ablum=$(input_anything "请输入元数据标签-专辑名")
+    if [ $? -eq 10 ]; then
+        return 20
+    fi
+
+    draw_line_echo "~"
+    text_echo "当前已设置元数据标签-专辑名为「$audio_ablum」"
+
+    log_start
+    make_directory "$output_path"
+    local operation_count=0
+    shopt -s nullglob
+    draw_line_echo "~"
+    for file in $(file_extension_for_loop "mp3" "m4a" "flac"); do
+        ffmpeg_no_banner -i "$file" -c copy -map_chapters -1 -metadata album="$audio_ablum" "$output_path/$file"
+        ((operation_count++))
+        show_progress_bar "$all_count" "$operation_count"
+    done
+    log_end "$operation_count" "$all_count"
+    log_result "$feature_note"
+    shopt -u nullglob
+}
