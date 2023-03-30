40bd5e1b0cb37bdad145607b7a06ff9da95e494a
Mr. Kin <im.misterkin@gmail.com>
2023-03-30 11:23:17 +0800

    refactor: update modules for the new refactor

---
+++ ffmpeg_modules/video_converter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+video_converter() {
+    local -a menu_options_array
+    menu_options_array+=("flv格式转mp4格式")
+    menu_options_array+=("退出子菜单")
+    while true; do
+        clear
+        draw_line_echo "="
+        echo -e "FFmpeg批处理工具子菜单 —— 视频转换器：视频格式互转\n"
+        echo "提示：使用上下方向键↑↓选择，回车键Enter确认选项："
+        echo -e "\n请选择子菜单：\n"
+        arrow_select_option "${menu_options_array[@]}"
+        local choice=$?
+        case ${menu_options_array[$choice]} in
+        "flv格式转mp4格式")
+            while [ $? -ne 20 ]; do
+                video_converter_flv2mp4
+            done
+            ;;
+        "退出子菜单")
+            return 20
+            ;;
+        esac
+    done
+}
--- ffmpeg_modules/video_converter_flv2mp4.sh
+++ ffmpeg_modules/video_converter_flv2mp4.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-flv_to_mp4() {
+video_converter_flv2mp4() {
     local feature_name feature_intro feature_note
     feature_name="flv格式转mp4格式"
-    feature_intro="将所有flv文件转换为mp4文件，仅仅是转换封装格式，不涉及重新编码音视频"
+    feature_intro="将路径下的flv文件转换为mp4文件，仅仅是转换封装格式，不涉及重编码"
     feature_note="$(description_append_note "option_false" "file" "mp4")"
     description "$feature_name" "$feature_intro" "$feature_note"
     change_directory
@@ -21,14 +21,13 @@ flv_to_mp4() {
     log_start
     local operation_count=0
     shopt -s nullglob
-    for file in *.flv *.FLV; do
+    for file in $(file_extension_for_loop "flv"); do
         detect_and_remove "file" "${file%.*}.mp4"
-        draw_line_echo "~"
-        ffmpeg_no_banner -i "$file" -c copy "${file%.*}.mp4"
+        ffmpeg_no_banner -i "$file" -c copy "$(get_file_name "$file").mp4"
         ((operation_count++))
-        echo
+        show_progress_bar "$all_count" "$operation_count"
     done
-    shopt -u nullglob
     log_end "$operation_count" "$all_count"
     log_result "$feature_note"
+    shopt -u nullglob
 }
