3018172eb2d35bcbb4b17adf1882951ee70ceeeb
Mr. Kin <im.misterkin@gmail.com>
2026-01-03 17:05:56 +0800

    fix: failed to delete when audio has two cover attach

--- ffmpeg_modules/audio_cover_delete.sh
+++ ffmpeg_modules/audio_cover_delete.sh
@@ -31,7 +31,8 @@ audio_cover_delete() {
     show_progress_bar "$all_count" $(("$operation_count" + "$no_cover_count"))
     for file in $(file_extension_for_loop "mp3" "m4a" "flac"); do
         media_stream_number=$(get_media_info "$file" "format=nb_streams")
-        if [ "$media_stream_number" -eq 2 ]; then
+        # 修改为>=2，避免一些音频文件附加了多个cover流时无法删除封面图的问题
+        if [ "$media_stream_number" -ge 2 ]; then
             # -map 0 选择所有流；-map -0:v 选择音频（-0:v加负号是取消选择视频流，即反选）
             ffmpeg_no_banner -i "$file" -map 0 -map -0:v -c copy "${output_path}/${file}"
             ((operation_count++))
